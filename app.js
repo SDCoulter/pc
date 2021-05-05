eaeafab669f2db81534db2066c64aba7369369ba
Shaun Coulter <shaundcoulter@gmail.com>
2021-05-05 18:56:44 -0400

    working on data validation - issues still with validateRaw

--- app.js
+++ app.js
@@ -4,37 +4,23 @@ function processData() {
   let nominal = d3.select("#nominal").node().value;
   let lower = d3.select("#lower").node().value;
   let rawData = d3.select("#rawdata").node().value.split(/\s+/);
-  // Remove last item if empty string.
-  if (rawData[rawData.length-1] == "") {
-    rawData.pop();
+
+  // Clear error message if there is one.
+  d3.select("#error-message").text("");
+
+  // Clean up and validate the rawData and limits.
+  rawData = cleanRaw(rawData);
+  var limits = checkLimits([upper, nominal, lower]);
+  validateRaw(rawData, limits[0], limits[2]);
+
+  // Only run the rest of the analysis if there is no error.
+  console.log(d3.select("#error-message").text())
+  if (d3.select("#error-message").text() == "" && rawData.length > 1 && limits.length == 3) {
+    console.log("good to go");
+  } else {
+    d3.select("#error-message").text("boobies");
   };
 
-  // Turn rawData to integers.
-
-  // parseInt is returning NaN values and not triggering error.
-  // needs to be fixed.
-
-  try {
-    rawData = rawData.map((val) => parseInt(val));
-  } catch (e) {
-    d3.select("#error-message").text(e + "THE ENTERED RAW DATA ARE NOT USABLE");
-  }
-
-  ///////////////////////////
-
-  // Check entered data for non-conformance.
-  if (!checkLimits(upper, nominal, lower)) {
-    d3.select("#error-message").text("THE ENTERED LIMITS ARE NOT USABLE");
-  }
-  if (!checkRawData(upper, lower, rawData)) {
-    d3.select("#error-message").text("THE ENTERED RAW DATA ARE NOT USABLE");
-  }
-
-  //console.log(checkLimits(upper, nominal, lower));
-  console.log(checkRawData(upper, lower, rawData));
-  // Store rawData as integers.
-  rawData = rawData.map((val) => parseInt(val));
-  //console.log(rawData);
   /*
   // Store all data in Object - run functions if required.
   var data = {
@@ -55,31 +41,65 @@ function processData() {
   */
 };
 
-function checkLimits(u, n, l) {
-  let arr = [u, n, l];
-  for (var i = 0; i < arr.length; i++) {
-    // Check values are between 0-250.
-    if (arr[i] < 0 || arr[i] > 250) {
-      return false
-    };
-  };
-  return true
-};
 
-function checkRawData(u, l, rd) {
-  // Return false if any other errors.
+function cleanRaw(rd) {
+  // Return the error if there is one.
   try {
-    // Check if values outside scope.
+    // Make a copy of the data.
+    var rawD = rd;
 
-    //////////////////// returning false when values are correct.
+    // Remove last item if empty string.
+    if (rawD[rawD.length-1] == "") {
+      rawD.pop();
+    };
+    // Turn rawData to integers.
+    rawD = rawD.map((val) => parseFloat(val));
 
-    for (var i = 0; i < rd.length; i++) {
-      if (rd[i] > u || rd[i] < l) {
-        return false
+    // Check every value in the array is a float.
+    for (var i in rawD) {
+      var value = rawD[i];
+      if (typeof value != "number" || value != value) {
+        d3.select("#error-message").text("THE ENTERED RAW DATA IS NOT VALID");
+        break;
       };
     };
   } catch (e) {
-    return false
+    // Display error.
+    d3.select("#error-message").text(e + " - ERROR FOUND");
   };
-  return true
+  // Return the cleaned data.
+  return rawD;
+};
+
+
+function checkLimits(lims) {
+  let arr = lims;
+  // Turn the values into floats.
+  arr = arr.map((val) => parseFloat(val));
+  // Make sure all values are floats (not NaN etc).
+  for (var i in arr) {
+    var value = arr[i];
+    if (typeof value != "number" || value != value) {
+      d3.select("#error-message").text("THE ENTERED LIMITS ARE NOT VALID");
+      break;
+    };
+  };
+  // Check order is correct.
+  if (arr[1] < arr[0] || arr[2] < arr[1] || arr[2] < arr[0] || arr[0] > arr[1]) {
+    d3.select("#error-message").text("THE ENTERED LIMITS ARE NOT VALID");
+  };
+  return arr;
+};
+
+
+function validateRaw(rd, u, l) {
+  // Check none of the raw data is outside the limits.
+  for (let i in rd) {
+    if (rd[i] > u || rd[i] < l) {
+      console.log(rd[i], u);
+      console.log(rd[i], l);
+      d3.select("#error-message").text("THE ENTERED DATA ARE NOT VALID");
+      break;
+    };
+  };
 };
