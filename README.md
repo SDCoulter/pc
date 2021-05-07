8de21f3dfde1d611ab4253ee1c5d2edbf1bb0771
Shaun Coulter <shaundcoulter@gmail.com>
2021-05-06 20:31:33 -0400

    populate average, max, min, and range

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ A look at Process Capability and if we can create a web tool to learn something.
 
 Using knowledge picked up during my day job I have endeavoured to delve more into understanding Process Capability in a manufacturing environment. My goal is to make a usable webpage that takes in upper and lower bounds and 30 points of raw data and provides dynamic analysis on this data. I plan to provide the general statistic values expected when dealing with Process Capability as well as graphs using Plotly.
 
-To read more about Process Capability please see: ![Engineering Statistics Handbook - Process Capability](https://www.itl.nist.gov/div898/handbook/pmc/section1/pmc16.htm)
+To read more about Process Capability please see: [Engineering Statistics Handbook - Process Capability](https://www.itl.nist.gov/div898/handbook/pmc/section1/pmc16.htm)
--- app.js
+++ app.js
@@ -16,25 +16,13 @@ function processData() {
   // Only run the rest of the analysis if there is no error.
   if (d3.select("#error-message").text() == "" && rawData.length > 1 && limits.length == 3) {
     console.log("Data Validation Passed");
-    /*
-    // Store all data in Object - run functions if required.
-    var data = {
-      average: getAverage(),
-      stdev: getStdev(),
-      max:
-      median:
-      min:
-      range:
-      cpk: getCpk(),
-      cr: getCr(),
-      cp: getCp(),
-      cpu: getCpu(),
-      cpl: getCpl(),
-      cpm: getCpm()
-    };
+
+    // Store all data in Object.
+    var data = findStats(rawData, upper, nominal, lower);
+    console.log(data);
     // Then populate the values of the divs with the matching names.
-    */
-  };  
+    populateFields(data);
+  };
 };
 
 
@@ -96,4 +84,81 @@ function validateRaw(rd, u, l) {
       break;
     };
   };
+};
+
+
+function findStats(rd, u, n, l) {
+  // Populate the stats with functions.
+  let stats = {
+    average: getAverage(rd),
+    //stdev: getStdev(),
+    max: Math.max(...rd),
+    //median: getMedian(rd),
+    min: Math.min(...rd),
+    range: getRange(rd),/*
+    cpk: getCpk(rd, u, l),
+    cr: getCr(),
+    cp: getCp(),
+    cpu: getCpu(),
+    cpl: getCpl(),
+    cpm: getCpm()*/
+  };
+
+  return stats
+};
+
+// Data functions start here.
+function getAverage(rd) {
+  let len = rd.length;
+  let sum = 0;
+  rd.forEach((val) => sum += val);
+  return sum / len;
+};
+function getStdev(rd) {
+
+};
+function getRange(rd) {
+  // Max number minus min number.
+  return Math.max(...rd) - Math.min(...rd);
+}
+function getMedian(rd) {
+
+};
+function getCpk(rd, u, l) {
+
+};
+function getCr() {
+
+};
+function getCp() {
+
+};
+function getCpu() {
+
+};
+function getCpl() {
+
+};
+function getCpm() {
+
+};
+// Data functions end here.
+
+
+
+
+function populateFields(data) {
+  // Fill the empty fields with data.
+  d3.select("#average").text(data.average);
+  //d3.select("#stdev").text(data.stdev);
+  d3.select("#max").text(data.max);
+  //d3.select("#median").text(data.median);
+  d3.select("#min").text(data.min);
+  d3.select("#range").text(data.range);
+  //d3.select("#cpk").text(data.cpk);
+  //d3.select("#cr").text(data.cr);
+  //d3.select("#cp").text(data.cp);
+  //d3.select("#cpu").text(data.cpu);
+  //d3.select("#cpl").text(data.cpl);
+  //d3.select("#cpm").text(data.cpm);
 };
